65c147bd09ebd602e398b555c4a93470219dee28
Milad <63479762+MRyderOC@users.noreply.github.com>
2021-10-06 22:29:29 -0500

    app initialization

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
